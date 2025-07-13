109f880f36ceb75e7bf749f77bc32c70777d2499
seweex <seweex@gmail.com>
2025-07-14 01:18:58 +0300

    Add object, scene and base of systems classes

---
+++ src/game/components/layer.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Seweex on 12.07.2025.
+//
+
+#include "../../../include/coli/game/Layer.h"
+
+namespace Coli {
+namespace Game {
+namespace Components {
+} // Components
+} // Game
+} // Coli
