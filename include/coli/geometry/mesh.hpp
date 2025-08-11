0fd813ffe955a43f3795f0fb234b7c2fa2eedf1f
seweex <seweex@gmail.com>
2025-08-11 19:17:44 +0300

    Rework Mesh docs

--- include/coli/geometry/mesh.hpp
+++ include/coli/geometry/mesh.hpp
@@ -5,21 +5,31 @@
 
 namespace Coli::Geometry
 {
+    /**
+     * @brief Vertex mesh class.
+     * @details Contains vertices that makes geometry mesh.
+     *
+     * @tparam VertexTy Vertex type. Must have a position and texcoord.
+     *
+     * @note Not thread-safe. Concurrent access requires
+     * external synchronization.
+    */
     template <Detail::VertexType VertexTy>
     class Mesh final
     {
     public:
         /**
-         * @brief Creates the mesh of unique vertices and indices from
-         * non-unique vertices sequence
+         * @brief Makes mesh from vertices array.
+         * @details Creates the mesh of unique vertices and indices from
+         * a non-unique vertices sequence.
          *
-         * @tparam Iter The type of input iterator to non-unique vertices sequence
+         * @tparam Iter Input iterator type to a non-unique vertices sequence.
          *
-         * @param first The beginning of the sequence
-         * @param last The end of the sequence
+         * @param first The beginning of the sequence;
+         * @param last The end of the sequence.
          *
-         * @throw std::bad_alloc If allocation fails
-         * @warning UB If passed invalid iterators
+         * @throw std::bad_alloc If allocation fails.
+         * @warning It's UB if passed invalid iterators.
          */
         template <std::input_iterator Iter>
             requires (std::same_as<VertexTy, std::remove_cvref_t<decltype(*std::declval<Iter>())>>)
@@ -45,15 +55,16 @@ namespace Coli::Geometry
         }
 
         /**
-         * @brief Copies the unique sequence of vertices and indices
+         * @brief Makes mesh from unique vertices and indices.
+         * @details Copies the unique sequence of vertices and indices to the mesh.
          *
-         * @tparam VertIter The type of input iterator to unique vertices sequence
-         * @tparam IdxIter The type of input iterator to index sequence
+         * @tparam VertIter Input iterator type to a unique vertices sequence;
+         * @tparam IdxIter Input iterator type to an index sequence.
          *
-         * @param vertexFirst The beginning of the unique vertex sequence
-         * @param vertexLast The end of the unique vertex sequence
-         * @param indexFirst The beginning of the index sequence
-         * @param indexLast The end of the index sequence
+         * @param vertexFirst The beginning of the unique vertex sequence;
+         * @param vertexLast The end of the unique vertex sequence;
+         * @param indexFirst The beginning of the index sequence;
+         * @param indexLast The end of the index sequence.
          */
         template <std::input_iterator VertIter, std::input_iterator IdxIter> requires (
             std::same_as<VertexTy, std::remove_cvref_t<decltype(*std::declval<VertIter>())>> &&
@@ -65,56 +76,72 @@ namespace Coli::Geometry
         {}
 
         /**
-         * @brief Moves the unique sequence of vertices and indices
+         * @brief Moves unique vertices and indices.
+         * @details Moves a unique sequence of vertices and indices to the mesh.
          *
-         * @param vertices The rvalue ref to unique vertices vector
-         * @param indices The rvalue ref to inices vector
+         * @param vertices R-value ref to a unique vertices vector;
+         * @param indices R-value ref to an inices vector.
          */
         Mesh(std::vector<VertexTy>&& vertices, std::vector<unsigned>&& indices) :
             myVertices (std::move(vertices)),
             myIndices (std::move(indices))
         {}
 
-        /// @brief Default copy-ctor
-        Mesh(Mesh const&) = default;
-
-        /// @brief Default move-ctor
-        Mesh(Mesh&&) noexcept = default;
+        /**
+         * @brief Copies mesh.
+         * @details Just copies the mesh.
+         *
+         * @param other Other mesh.
+         */
+        Mesh(Mesh const& other) = default;
 
         /**
-         * @brief Default copy assignment
-         * @return The reference to itself
+         * @brief Moves mesh.
+         * @details Just moves the mesh.
+         *
+         * @param other Other mesh.
          */
+        Mesh(Mesh&& other) noexcept = default;
+
+        /// @copydoc Mesh(Mesh const&)
         Mesh& operator=(Mesh const&) = default;
 
-        /**
-         * @brief Default move assignment
-         * @return The reference to itself
-         */
+
+        /// @copydoc Mesh(Mesh&&)
         Mesh& operator=(Mesh&&) noexcept = default;
 
-        /// @brief Destroys the mesh
+        /**
+         * @brief Destroys mesh.
+         * @details Just destroys the mesh.
+         */
         ~Mesh() noexcept = default;
 
         /**
-         * @brief Gets the currently stored vertices
-         * @return The const span to storing vertices
+         * @brief Returns vertices.
+         * @details Returns the currently stored vertices in the mesh.
+         * The returned span contains unique vertices.
+         *
+         * @return Span of unique vertices stored in the mesh.
          */
         [[nodiscard]] std::span<VertexTy const> get_vertices() const noexcept {
             return myVertices;
         }
 
         /**
-         * @brief Gets the currently stored indices
-         * @return The const span to storing indices
+         * @brief Returns indices.
+         * @details Returns the currently stored indices in the mesh.
+         *
+         * @return Span of indices stored in the mesh.
          */
         [[nodiscard]] std::span<unsigned const> get_indices() const noexcept {
             return myIndices;
         }
 
         /**
-         * @brief Gets the number of the currently stored non-unique vertices
-         * @return The number of storing non-unique vertices
+         * @brief Returns number of vertices.
+         * @details Returns the currently stored indices count in the mesh.
+         *
+         * @return Number of indices stored in the mesh.
          */
         [[nodiscard]] size_t size() const noexcept {
             return myIndices.size();
