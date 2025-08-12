76fb2106cb0664010a2e01f83fc5aee0dc637221
seweex <seweex@gmail.com>
2025-08-12 16:09:03 +0300

    Rework Program docs

--- include/coli/graphics/program.h
+++ include/coli/graphics/program.h
@@ -3,14 +3,12 @@
 
 #include "coli/graphics/shader.h"
 
+/**
+ * @brief For internal details.
+ * @note The user should not use this namespace.
+ */
 namespace Coli::Graphics::Detail::inline OpenGL
 {
-    /**
-     * @brief A factory class for creating OpenGL programs
-     *
-     * @warning This class and all its methods are not supposed to be used by the user
-     * @note This class does not allow instances
-     */
     class ProgramFactory final
     {
         [[noreturn]] static void fail_initialization_error();
@@ -19,30 +17,8 @@ namespace Coli::Graphics::Detail::inline OpenGL
         [[noreturn]] static void fail_link_error();
 
     public:
-        /**
-         * @brief Destroys the shader
-         *
-         * @param context The context that was used to create a program
-         * @param handle The program handle
-         */
         static void destroy(Graphics::OpenGL::Context& context, GLuint handle) noexcept;
 
-        /**
-         * @brief Creates an OpenGL program
-         *
-         * @param context The valid context to check it loaded
-         * @param vertex A vertex shader
-         * @param fragment A fragment shader
-         *
-         * @throw std::invalid_argument If the context is invalid
-         * @throw std::invalid_argument If the vertex shader is invalid
-         * @throw std::invalid_argument If the fragment shader is invalid
-         *
-         * @throw std::logic_error If calls not from the context creation thread
-         * @throw std::runtime_error If initialization fails
-         *
-         * @return A valid raw handle to the newly created program
-         */
         [[nodiscard]] static GLuint create(
             std::shared_ptr<Graphics::OpenGL::Context>& context,
             VertexShader const& vertex,
@@ -61,6 +37,7 @@ namespace Coli::Graphics::Detail::inline OpenGL
     };
 }
 
+/// @brief Namespace for the all OpenGL-based stuff.
 namespace Coli::Graphics::inline OpenGL
 {
     /// @brief OpenGL program class
@@ -74,55 +51,61 @@ namespace Coli::Graphics::inline OpenGL
 
     public:
         /**
-        * @brief Creates a valid program and links it with your
-        * shaders
-        *
-        * @param context The valid context to check it is loaded
-        * @param vertex The vertex shader
-        * @param fragment The fragment shader
-        *
-        * @throw std::invalid_argument If the context is invalid
-        * @throw std::invalid_argument If the vertex shader is invalid
-        * @throw std::invalid_argument If the fragment shader is invalid
-        *
-        * @throw std::logic_error If calls not from the context creation thread
-        * @throw std::runtime_error If initialization fails
-        */
-        Program(
-           std::shared_ptr<OpenGL::Context> context,
-           VertexShader const& vertex,
-           FragmentShader const& fragment
-       );
+         * @brief Creates program.
+         * @details Creates a valid program from the user's shaders.
+         *
+         * @param context The valid context to check it is loaded;
+         * @param vertex Vertex shader;
+         * @param fragment Fragment shader.
+         *
+         * @throw std::invalid_argument If the context is invalid;
+         * @throw std::invalid_argument If the vertex shader is invalid;
+         * @throw std::invalid_argument If the fragment shader is invalid;
+         *
+         * @throw std::logic_error If calls not from the context creation thread;
+         * @throw std::runtime_error If initialization fails.
+         */
+         Program(
+            std::shared_ptr<OpenGL::Context> context,
+            VertexShader const& vertex,
+            FragmentShader const& fragment
+        );
 
         /**
-         * @brief Accepts a moved program
+         * @brief Moves program.
+         * @detail Moves the program.
          *
-         * @throw std::logic_error If moved from other thread
+         * @throw std::logic_error If moved from other thread;
          */
         Program(Program&&) noexcept(false);
 
-        /**
-         * @copydoc Program(Program&&)
-         * @return A reference to itself
-         */
+        /// @copydoc Program(Program&&)
         Program& operator=(Program&&) noexcept(false);
 
         Program(const Program&) = delete;
         Program& operator=(const Program&) = delete;
 
-        /// @brief Deletes the shader
+        /**
+         * @brief Destroys program.
+         * @detail Destroys the program.
+         */
         ~Program() noexcept;
 
         /**
-         * @brief Binds the program to the graphic pipeline
+         * @brief Binds program.
+         * @brief Binds the program to the graphic pipeline.
          *
-         * @throw std::invalid_argument If calls on the invalid object
-         * @throw std::logic_error If calls not from the context creation thread
-         * @throw std::logic_error If another program is bound
+         * @throw std::invalid_argument If calls on the invalid object;
+         * @throw std::logic_error If calls not from the context creation thread;
+         * @throw std::logic_error If another program is bound.
          */
         void bind();
 
-        /// @brief Unbinds the bound program
+        /**
+         * @brief Unbinds program.
+         * @brief Unbinds the program from the graphic pipeline if
+         * the bound program is current.
+         */
         void unbind() noexcept;
     };
 }
